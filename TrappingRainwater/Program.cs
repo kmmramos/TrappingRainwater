c0a088ba6c5e02a67e6db193adb09cb1b43f5cfe
Kirstin Megga Ramos <kirstinmegga.ramos@ph.softvision.global>
2022-06-05 21:49:37 +0800

    first commit

---
+++ TrappingRainwater/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Array_SumOfTarget
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            try
+            {
+                Console.Write("Please enter the size of your array: ");
+                int size = Convert.ToInt32(Console.ReadLine());
+                int[] heights = new int[size];
+                for (int i = 0; i < heights.Length; i++)
+                {
+                    Console.Write("Please enter your number: ");
+                    heights[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                int answer;
+                answer = GetTrappedRainwater(heights);
+                Console.WriteLine("Here is the total area of the trapped rainwater: {0}", answer.ToString());
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        //NORMAL SOLUTION
+        public static int GetTrappedRainwater(int[] heights)
+        {
+            int totalWater = 0;
+            for (int p = 0; p < heights.Length; p++)
+            {
+                int leftP = p, rightP = p, maxLeft = 0, maxRight = 0;
+
+                //determine maxLeft
+                while (leftP >= 0)
+                {
+                    maxLeft = Math.Max(maxLeft, heights[leftP]);
+                    leftP--;
+                }
+
+                //determine maxRight
+                while (rightP < heights.Length)
+                {
+                    maxRight = Math.Max(maxRight, heights[rightP]);
+                    rightP++;
+                }
+
+                int currentWater = Math.Min(maxLeft, maxRight) - heights[p];
+                if (currentWater >= 0)
+                {
+                    totalWater += currentWater;
+                }
+            }
+            return totalWater;
+        }
+
+    }
+}
